68b3a4a9223ca634dd12a01c29cc3d746364980c
ifnicholaskotzias <ifnicholaskotzias@gmail.com>
2026-06-08 23:17:39 -0300

    feat: create AnimalRequest to handle validation for animal-related data

---
+++ sigar-api/app/Http/Requests/AnimalRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AnimalRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nome' => ['nullable','string','max:255'],
+            'especie' => ['required','string','max:100'],
+            'raca' => ['nullable','string','max:100'],
+            'sexo' => ['nullable','in:M,F,N/A'],
+            'data_nascimento' => ['nullable','date'],
+            'peso_atual' => ['nullable','numeric','min:0'],
+            'alergia' => ['nullable','string'],
+            'status' => ['required', 'in:RESGATADO,ADOTADO,OBITO'],
+            'foto_path' => ['nullable','string'],
+            'observacoes_gerais' => ['nullable','string']
+        ];
+    }
+}
