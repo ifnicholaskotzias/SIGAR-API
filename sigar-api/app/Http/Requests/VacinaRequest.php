5f0ebcf3f1e59e8d08b055c4c67435a10dc98318
ifnicholaskotzias <ifnicholaskotzias@gmail.com>
2026-06-11 19:07:57 -0300

    feat: create VacinaRequest to handle validation for Vacina-related data

---
+++ sigar-api/app/Http/Requests/VacinaRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class VacinaRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nome' => ['required', 'string', 'max:255'],
+            'fabricante' => ['nullable', 'string', 'max:255'],
+            'doenca_alvo' => ['nullable', 'string', 'max:255'],
+            'frequencia_dias' => ['nullable', 'integer'],
+            'observacoes' => ['nullable', 'string'],
+        ];
+    }
+}
