bbfda3bea6e9e85f5c2187e3080109cafd193ba0
ifnicholaskotzias <ifnicholaskotzias@gmail.com>
2026-06-16 20:41:05 -0300

    feat: add API routes for UsuarioController

--- sigar-api/routes/api.php
+++ sigar-api/routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\MedicamentoController;
 use App\Http\Controllers\VacinaController;
+use App\Http\Controllers\UsuarioController;
 
 Route::apiResource('animais', AnimalController::class);
 Route::apiResource('medicamentos', MedicamentoController::class);
 Route::apiResource('vacinas', VacinaController::class);
+Route::apiResource('usuarios', UsuarioController::class);
